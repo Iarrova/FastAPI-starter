d270df025d67a3d20e4739c96b81362ac08b8b6b
Iarrova <iroberts1398@gmail.com>
2026-06-16 18:14:01 -0400

    chore: add tablename to user model

--- app/models/user.py
+++ app/models/user.py
@@ -5,6 +5,8 @@ from sqlmodel import Field, SQLModel
 
 
 class User(SQLModel, table=True):
+    __tablename__ = "users"
+
     id: uuid.UUID | None = Field(default_factory=uuid.uuid4, primary_key=True)
     name: str
     email: EmailStr = Field(unique=True)
